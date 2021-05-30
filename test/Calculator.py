8d1ce9114cedb21f615a7641bd11ef904a5186db
hayley618 <763951606@qq.com>
2021-05-30 22:01:34 +0800

    first commit

---
+++ test/Calculator.py
@@ -0,0 +1,18 @@
+class Cal:
+    # 相加
+    def add(self, a, b):
+        return a+b
+
+    # 相除
+    def div(self, a, b):
+        return a / b
+
+    # 相减
+    def sub(self, a, b):
+        return a - b
+
+    # 相乘
+    def multi(self, a, b):
+        return a * b
+# cal=Calculator()
+# print(cal.add(1, 2))
