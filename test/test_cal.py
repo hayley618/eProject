dcc2c3b74036846e6db22a1c096ce9e5bfd7e3e1
hayley618 <763951606@qq.com>
2021-05-31 21:29:31 +0800

    pytest

--- test/test_cal.py
+++ test/test_cal.py
@@ -3,20 +3,22 @@ import yaml
 import Calculator
 
 data = yaml.safe_load(open("./data_cal.yaml"))
-# print(data["add"])
 
 class TestCal:
-    cal = Calculator.Cal()
+
+    def setup(self):
+        self.cal = Calculator.Cal()
+
     def setup_class(self):
         print("开始计算")
 
     def teardown_class(self):
         print("计算结束")
-     # 相加
+    # 相加
     @pytest.mark.parametrize('a,b,expect', data["add"])
     def test_add(self, a, b, expect):
         result = self.cal.add(a, b)
-        assert expect == result
+        assert expect == round(result, 2)
         print("a+b=", result)
 
     # 相除
@@ -24,7 +26,7 @@ class TestCal:
     def test_div(self, a, b, expect):
         try:
             result = self.cal.div(a, b)
-            assert expect == result
+            assert expect == round(result, 2)
             print("a/b=", result)
         except ZeroDivisionError:
             print("fail，除数不能为0")
@@ -33,12 +35,12 @@ class TestCal:
     @pytest.mark.parametrize('a,b,expect', data["sub"])
     def test_sub(self, a, b, expect):
         result = self.cal.sub(a, b)
-        assert expect == result
+        assert expect == round(result, 2)
         print("a-b=", result)
 
     # 相乘
     @pytest.mark.parametrize('a,b,expect', data["multi"])
     def test_multi(self, a, b, expect):
         result = self.cal.multi(a, b)
-        assert result - expect <= 0.00000000001
+        assert expect == round(result, 2)
         print("a*b=", result)
